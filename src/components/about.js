79e44edd0d430ead13500da297546e734c698853
Nathan Yordanos Tadesse <natetadesse4991@gmail.com>
2020-07-21 11:04:35 -0400

    added bold text

--- src/components/about.js
+++ src/components/about.js
@@ -16,7 +16,7 @@ class About extends React.Component {
                 Since childhood, I have always been passionate about technology and how it can be implemented to solve a vast array of problems. I remember sitting up late at night, watching my older cousin work on developing websites for his clients. Fast forward to adulthood and I have made both apps and websites that exemplify both User Experience and Website Functionality.
 
 
-I look forward to connecting with anyone who is passionate about technology, and Front End Development. Please do not hesitate to message me here, or through my email at n.tadesse94@gmail.com 
+I look forward to connecting with anyone who is passionate about technology, and Front End Development. Please do not hesitate to message me here, or through my email at <b>natetadesse4991@gmail.com</b>.
                 </p>
                 <br></br>
                 <br></br>
